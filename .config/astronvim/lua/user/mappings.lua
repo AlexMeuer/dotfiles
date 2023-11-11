b9a2e9566a8e68b906c416a55d0c61709bebd966
Alex Meuer <alex@alexmeuer.com>
2023-11-11 18:47:40 +0000

    [Nvim] Add mappings for spotify control

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -3,6 +3,7 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+local utils = require "astronvim.utils"
 return {
   -- first key is the mode
   n = {
@@ -19,6 +20,33 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     ["<leader>fp"] = { "<cmd>Telescope projects<cr>", desc = "Find Projects" },
+    ["<leader>ts"] = {
+      -- Assumes that toggle term is available.
+      function() utils.toggle_term_cmd "spt" end,
+      desc = "ToggleTerm spotify",
+    },
+    ["<leader>m"] = { desc = "Music" },
+    ["<leader>mo"] = {
+      -- Assumes that toggle term is available.
+      function() utils.toggle_term_cmd "spt" end,
+      desc = "ToggleTerm spotify",
+    },
+    ["<leader>mt"] = {
+      function() vim.fn.jobstart "spt playback --toggle" end,
+      desc = "Toggle play/pause",
+    },
+    ["<leader>mn"] = {
+      function() vim.fn.jobstart "spt playback --next" end,
+      desc = "Next song",
+    },
+    ["<leader>mp"] = {
+      function() vim.fn.jobstart "spt playback --previous" end,
+      desc = "Previous song",
+    },
+    ["<leader>ms"] = {
+      function() vim.fn.jobstart "spt playback --shuffle" end,
+      desc = "Toggle shufle",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
